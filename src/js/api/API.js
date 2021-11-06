60c304ceaf0426ebb74f1df425f8c883f14f034f
Dima Sotskiy <sotskiydr@gmail.com>
2021-11-07 01:24:28 +0200

    add api service (demo)

---
+++ src/js/api/API.js
@@ -0,0 +1,41 @@
+const KEY = '9b0c2f9965f33f91e75ff619d689bb58';
+
+export default class FetchData {
+  constructor() {
+    this.page = 1;
+  }
+  // Получаем популярное
+  async getTrandingMovie(page) {
+    const trandingData = `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}&page=${this.page}`;
+    try {
+      const result = await fetch(`${trandingData}`);
+      const response = result.json();
+      return response;
+    } catch (err) {
+      console.log('error')
+    }
+  };
+  // Получаем фильм по значению
+  async getQueryMovie (query){
+    const queryValue = `&query=${query}`;
+    const queryData = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&page=${this.page}&include_adult=false`;
+    try {
+      const result = await fetch(`${queryData}${queryValue}`);
+      const response = result.json();
+      return response;
+    } catch (err) {
+      console.log('error')
+    }
+  }
+  //получем фильм по id
+  async getDescriptionMovie (id){
+    const queryData = `https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=en-US`;
+    try {
+      const result = await fetch(`${queryData}`);
+      const response = result.json();
+      return response;
+    } catch (err) {
+      console.log('error')
+    }
+  }
+}
---
+++ src/js/example-api.js
@@ -0,0 +1,7 @@
+import API from '../js/api/API'
+
+const fetchData = new API();
+
+fetchData.getTrandingMovie().then(data => console.log(data));
+fetchData.getQueryMovie('batman').then(data => console.log(data));
+fetchData.getDescriptionMovie('321528').then(data => console.log(data));
