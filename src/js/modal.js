e21ea683ba08dec3bdcbd924d6b06a9ca3dad413
Andrii-OttO <82340217+Andrii-OttO@users.noreply.github.com>
2021-11-09 00:03:04 +0300

    доработка
    адаптивная графика и включение и выключение модаки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,22 @@
-const galleryPosterSetModal = document.querySelector(".gallery__poster");
-const closeBtn = document.querySelector('.modal__button_close')
+const galleryPosterSetModal = document.querySelector(".js-gallery");
+const closeBtn = document.querySelector(".modal__button_close")
 const modalBackdrop = document.querySelector(".modal_backdrop");
+////////////
 
+galleryPosterSetModal.addEventListener('click', open);
+closeBtn.addEventListener('click', close);
+//modalBackdrop.addEventListener('click', close);    //не работает на оверфлок закрытие
 
-const openDescriptionMovie = addEventListener("click", open);
-const closeDescription = addEventListener('click', close);
-
-function open(){
-    modalBackdrop.classList.remove('.is-hidden')
+function open(e){
+    if(e.currentTarget.classList !== ('gallery__img')){
+    }
+    modalBackdrop.classList.remove('is-hidden')
 }
 function close(){
-    modalBackdrop.classList.add('.is-hidden')   
+    modalBackdrop.classList.add('is-hidden')   
 }
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Escape'){
+        modalBackdrop.classList.add('is-hidden')
+    }
+})
