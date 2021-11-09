9f7955cea15795f021a9917ba967e2a75b89ed57
Наталия Фарафонова <ferymfon1983@gmail.com>
2021-11-09 13:10:50 +0200

    Создала список членов команды

---
+++ src/js/heroes.js
@@ -0,0 +1,55 @@
+import photoFarafonova from "../images/team/farafonova.jpg";
+import photowoman from "../images/team/womenavatar.jpg";
+import photoman from "../images/team/menavatar.jpg";
+
+
+
+export default [
+    {
+        name: 'Dima',
+        photo: photoman,
+        position: 'Team Lider'
+    },
+    
+    {
+        name: 'Eugenia',
+        photo: photowoman,
+        position: 'Scrum Master'
+    },
+    
+    {
+        name: 'Natalia',
+        photo: photowoman,
+        position: 'Developer'
+    },
+    
+    {
+        name: 'Alexandr',
+        photo: photoman,
+        position: 'Developer'
+    },
+    
+    {
+        name: 'Andrii',
+        photo: photoman,
+        position: 'Developer'
+    },
+     
+    {
+        name: 'Vitaliy',
+        photo: photoman,
+        position: 'Developer'
+    },
+       
+    {
+        name: 'Oleg',
+        photo: photoman,
+        position: 'Developer'
+    },
+       
+    {
+        name: 'Natalya',
+        photo: photoFarafonova,
+        position: 'Developer'
+    },
+];
