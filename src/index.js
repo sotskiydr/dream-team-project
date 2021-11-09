420e34493e53a8d169c111cc58ae2fa2e94c0662
Наталия Фарафонова <ferymfon1983@gmail.com>
2021-11-09 15:18:47 +0200

    Написала логику для секции команды

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import './js/example-api';
 import './js/load-popular-main';
 import './js/pagination';
+import './js/team';
--- src/js/team.js
+++ src/js/team.js
@@ -2,6 +2,7 @@ import teamList from './heroes';
 import heroTempL from '../templates/team.hbs';
 
 
+
 const refs = {
     body: document.querySelector('body'),
     teamSection: document.querySelector('.team__herous'),
@@ -9,16 +10,48 @@ const refs = {
     closeModalTeam: document.querySelector('.js-team__close-modal'),
     backdropTeam: document.querySelector('.js-team__backdrop'),
 
-};
+}
+
 
 refs.openModalTeam.addEventListener('click', onOpenModal);
 
+
+
 function onOpenModal(e) {
-    document.body.classList.add('.show-team');
+    refs.body.classList.add('show-team');
     e.preventDefault();
     renderTeam(teamList);
-};
+    window.addEventListener('keydown', onEscPress);
+    refs.closeModalTeam.addEventListener('click', onCloseModal);
+    refs.backdropTeam.addEventListener('click', onBackdrop);
+    
+    
+}
 
-function renderTeam () {
-    console.log('hello');
-};
+function renderTeam() {
+    const markUp = heroTempL(teamList);
+    refs.teamSection.innerHTML = markUp;
+    
+}
+
+function onCloseModal() {
+    window.removeEventListener('keydown', onEscPress);
+    document.body.classList.remove('show-team');
+    
+    refs.closeModalTeam.removeEventListener('click', onCloseModal);
+    refs.backdropTeam.removeEventListener('click', onBackdrop);
+    refs.teamSection.innerHTML = "";
+}
+
+function onBackdrop(e) {
+    if (e.currentTarget === e.target) {
+        onCloseModal();
+    }
+    
+}
+
+function onEscPress(e) {
+    if (e.code === 'Escape') {
+        onCloseModal();
+    }
+}
