f9ff50ed8fdc84330177563872660f545ab12d40
Наталия Фарафонова <ferymfon1983@gmail.com>
2021-11-09 13:47:39 +0200

    создала шаблон команды

--- src/js/team.js
+++ src/js/team.js
@@ -1,3 +1,7 @@
+import teamList from './heroes';
+import heroTempL from '../templates/team.hbs';
+
+
 const refs = {
     body: document.querySelector('body'),
     teamSection: document.querySelector('.team__herous'),
@@ -5,11 +9,16 @@ const refs = {
     closeModalTeam: document.querySelector('.js-team__close-modal'),
     backdropTeam: document.querySelector('.js-team__backdrop'),
 
-}
+};
 
 refs.openModalTeam.addEventListener('click', onOpenModal);
 
 function onOpenModal(e) {
     document.body.classList.add('.show-team');
     e.preventDefault();
-}
+    renderTeam(teamList);
+};
+
+function renderTeam () {
+    console.log('hello');
+};
