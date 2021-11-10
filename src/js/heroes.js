867bb2017471736af16e83f027019cd78264ec91
Наталия Фарафонова <ferymfon1983@gmail.com>
2021-11-10 13:14:41 +0200

    Анимация сердца

--- src/js/heroes.js
+++ src/js/heroes.js
@@ -8,48 +8,56 @@ export default [
     {
         name: 'Dima',
         photo: photoman,
-        position: 'Team Lider'
+        position: 'Team Lider',
+        linkedin: 'https://www.linkedin.com/',
     },
     
     {
         name: 'Eugenia',
         photo: photowoman,
-        position: 'Scrum Master'
+        position: 'Scrum Master',
+        linkedin: 'https://www.linkedin.com/',
     },
     
     {
         name: 'Natalia',
         photo: photowoman,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
     
     {
         name: 'Alexandr',
         photo: photoman,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
     
     {
         name: 'Andrii',
         photo: photoman,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
      
     {
         name: 'Vitaliy',
         photo: photoman,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
        
     {
         name: 'Oleg',
         photo: photoman,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
        
     {
         name: 'Natalya',
         photo: photoFarafonova,
-        position: 'Developer'
+        position: 'Developer',
+        linkedin: 'https://www.linkedin.com/',
     },
 ];
